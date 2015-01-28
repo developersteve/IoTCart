f870590fa9e7cb94f67e3e402318b2fbc3d81ad2
PP Laptop <steven@developersteve.com>
2015-01-28 15:39:05 +1100

    initial

---
+++ proc.php
@@ -0,0 +1,28 @@
+<?php
+
+require 'braintree.php';
+
+Braintree_Configuration::environment('sandbox');
+Braintree_Configuration::merchantId('23nd25g4kn7gnqbb');
+Braintree_Configuration::publicKey('8552x2ym5bvhsycp');
+Braintree_Configuration::privateKey('17f3279171d4fd90ee9cd5256be17abf');
+
+if ($_POST['amt'] && $_POST['nonce'] && $_POST['cart']) {
+
+	$result = Braintree_Transaction::sale(array(
+		'amount' => $_POST['amt'],
+		'paymentMethodNonce' => $_POST['nonce'],
+		'customFields' => array(
+			'cart' => $_POST['cart'],
+		),
+	));
+
+	if ($result->success == 1) {echo $result->transaction->id;} else {echo "failed";}
+
+} else {
+	$token = Braintree_ClientToken::generate(array(
+	));
+	echo $token;
+}
+
+?>
