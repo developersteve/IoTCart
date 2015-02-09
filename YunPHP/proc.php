a83efc759a17a1a588e07606249946e81b523624
PP Laptop <steven@developersteve.com>
2015-02-09 22:25:13 +1100

    updated with new PHP and Sketch

--- YunPHP/proc.php
+++ YunPHP/proc.php
@@ -1,4 +1,5 @@
 <?php
+
 require 'Braintree.php';
 
 Braintree_Configuration::environment('sandbox');
@@ -10,32 +11,46 @@ $now = new Datetime();
 $past = clone $now;
 $past = $past->modify("-1 days");
 
+class MyDB extends SQLite3
+{
+        function __construct()
+        {
+                $this->open('/www/proc/YunPHP/print.db');
+        }
+}
+
+$db = new MyDB();
+
 $lookup = Braintree_Transaction::search(array(
-	Braintree_CustomerSearch::createdAt()->between($past, $now),
+        Braintree_CustomerSearch::createdAt()->between($past, $now),
 ));
 
 foreach ($lookup->_ids as $ids) {
 
-	$record = Braintree_Transaction::find($ids);
+        $record = Braintree_Transaction::find($ids);
 
-	if (!file_exists("files/" . $ids) and $record->customFields) {
+        if (!file_exists("/www/proc/YunPHP/files/" . $ids) and $record->customFi
 
-		$fh = fopen("files/" . $ids, 'w');
+                $fh = fopen("/www/proc/YunPHP/files/" . $ids, 'w');
 
-		$cart = json_decode($record->customFields['cart'], true);
+                $cart = json_decode($record->customFields['cart'], true);
 
-		foreach ($cart as $key => $value) {
-			if (array_key_exists("total", $value)) {
-				fwrite($fh, "Total: $" . $value["total"]);
-			} else {
-				fwrite($fh, $value['qty'] . " " . $value["itemName"] . "\n");
-				fwrite($fh, "$" . $value["price"] . "\n\n");
-			}
-		}
+                fwrite($fh, "Order #" . $ids . "\n\n");
 
-		fclose($fh);
+                foreach ($cart as $key => $value) {
+                        if (array_key_exists("total", $value)) {
+                                fwrite($fh, "Total: $" . $value["total"]);
+                        } else {
+                                fwrite($fh, $value['qty'] . " " . $value["itemName"] . "\n");
+                                fwrite($fh, "$" . $value["price"] . "\n\n");
+                        }
+                }
 
-	}
+                fclose($fh);
+
+                $db->exec("INSERT INTO print (trans, ind) VALUES ('".$ids."', 0)");
+
+    }
 
 }
 
